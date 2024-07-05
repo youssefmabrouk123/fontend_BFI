5b0a25b132f1e2bee4fbef25d4d6af24ad284b3b
Zaineb271 <zainebabderrazak6@gmail.com>
2024-07-05 14:17:20 +0100

    my first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,11 +3,13 @@ import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
 import { HeaderComponent } from './header/header.component';
 import { FooterComponent } from './footer/footer.component';
+import { RecaptchaModule } from 'ng-recaptcha';
+
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet ,HeaderComponent,FooterComponent],
+  imports: [CommonModule, RouterOutlet ,HeaderComponent,FooterComponent,RecaptchaModule],
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,6 +1,10 @@
 import { Routes } from '@angular/router';
 import { HomeComponent } from './home/home.component';
+import { MonCompteComponent } from './mon-compte/mon-compte.component';
+import { NousContacterComponent } from './nous-contacter/nous-contacter.component';
 
 export const routes: Routes = [
-    {path:'' , component:HomeComponent}
+    {path:'' , component:HomeComponent},
+    {path:'mon-compte' , component:MonCompteComponent},
+    {path:'nous-contacter' , component:NousContacterComponent}
 ];
